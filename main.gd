baf29f760968c4d231c1db057caae64ec4c49949
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-05-26 17:50:38 -0500

    Added call backs for the buttons

--- main.gd
+++ main.gd
@@ -1,12 +1,39 @@
 extends Control
 
 var text_editor : TextEdit
+var text_editor_control : Control
+
+var save_button : Button
+var load_button : Button
+var calc_button : Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	save_button = %save_button
+	load_button = %load_button
+	calc_button = %calc_button
+
+	save_button.toggled.connect(cb_save_button)
+	load_button.toggled.connect(cb_load_button)
+	calc_button.toggled.connect(cb_calc_button)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func cb_save_button(toggled : bool) -> void:
+	print("save button")
+
+
+func cb_load_button(toggled : bool) -> void:
+	print("load button")
+
+
+func cb_calc_button(toggled : bool) -> void:
+	print("calc button")
+
+
+
+	
