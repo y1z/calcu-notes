08b6658a88bb88d997ab7daab97b6d7301c6e1af
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-05-26 17:53:34 -0500

    Changed from signal Toggled to Button_Down

--- main.gd
+++ main.gd
@@ -1,11 +1,12 @@
 extends Control
 
-var text_editor : TextEdit
-var text_editor_control : Control
+var text_editor: TextEdit
+var text_editor_control: Control
 
-var save_button : Button
-var load_button : Button
-var calc_button : Button
+var save_button: Button
+var load_button: Button
+var calc_button: Button
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,27 +14,21 @@ func _ready() -> void:
 	load_button = %load_button
 	calc_button = %calc_button
 
-	save_button.toggled.connect(cb_save_button)
-	load_button.toggled.connect(cb_load_button)
-	calc_button.toggled.connect(cb_calc_button)
+	save_button.button_down.connect(cb_save_button)
+	load_button.button_down.connect(cb_load_button)
+	calc_button.button_down.connect(cb_calc_button)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
-func cb_save_button(toggled : bool) -> void:
+
+func cb_save_button() -> void:
 	print("save button")
 
 
-func cb_load_button(toggled : bool) -> void:
+func cb_load_button() -> void:
 	print("load button")
 
 
-func cb_calc_button(toggled : bool) -> void:
+func cb_calc_button() -> void:
 	print("calc button")
-
-
-
-	
