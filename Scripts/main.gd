dbb1450db308c276616bac6f107f3f08c7a2f5b2
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-05-28 16:07:23 -0500

    main.gd Add logic for calculator

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -13,9 +13,34 @@ var text_editor_control: Control
 
 var calculator_control: Control
 
+## Universal UI
 var save_button: Button
 var load_button: Button
 var switch_button: Button
+
+## Calculator Buttons UI
+var button_0: Button
+var button_1: Button
+var button_2: Button
+var button_3: Button
+var button_4: Button
+var button_5: Button
+var button_6: Button
+var button_7: Button
+var button_8: Button
+var button_9: Button
+## Math operators
+var button_dot: Button;
+var button_equals: Button;
+var button_plus: Button;
+var button_minus: Button;
+var button_mul: Button;
+var button_div: Button;
+## screen operators
+var button_c: Button;
+var button_ac: Button;
+var button_prev: Button;
+var button_next: Button;
 
 var sql_manager: SQLManager
 
@@ -70,15 +95,39 @@ func cb_switch_button() -> void:
 
 
 func _set_variables():
+	#SQL
 	sql_manager = %sql_manager
+	# Universal UI
 	save_button = %save_button
 	load_button = %load_button
 	switch_button = %switch_button
+	#Text Editor
+	text_editor = %text_editor
+	text_editor_control = %text_editor_control
+	#Buttons
+	button_0 = %"0_button"
+	button_1 = %"1_button"
+	button_2 = %"2_button"
+	button_3 = %"3_button"
+	button_4 = %"4_button"
+	button_5 = %"5_button"
+	button_6 = %"6_button"
+	button_7 = %"7_button"
+	button_8 = %"8_button"
+	button_9 = %"9_button"
+	button_dot = %"dot_button"
+	button_equals = %"equal_button"
+	button_plus = %"plus_button"
+	button_minus  = %"minus_button"
+	button_mul = %"mul_button"
+	button_div = %"div_button"
+	button_c = %"c_button"
+	button_ac  = %"ac_button"
+	button_next  = %"next_button"
+	button_prev = %"prev_button"
 
 	calculator_control = %calculator_control
-	text_editor_control = %text_editor_control
-
-	text_editor = %text_editor
+	
 
 	var rng := RandomNumberGenerator.new()
 	var rand_quote_index: int = rng.randi_range(0, random_quotes.size() - 1)
