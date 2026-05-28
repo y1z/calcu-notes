41c70489429baeba3833ec504f889fbaed24ab9f
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-05-28 18:50:33 -0500

    main.gd Implemented calculator Logic

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,12 +6,38 @@ enum AppsToChose
 	Calculator,
 }
 
+enum MathOperator
+{
+	Plus,
+	Minus,
+	Mul,
+	Div,
+	Equal,
+}
+
+enum CalculatorState
+{
+	DoesNotHaveDot,
+	HasDot,
+}
+
+const MathOperatorToSymbol: Dictionary = {
+	MathOperator.Plus: "+",
+	MathOperator.Minus: "-",
+	MathOperator.Mul: "*",
+	MathOperator.Div: "/",
+	MathOperator.Equal: "=",
+}
+
+var calculator_state: CalculatorState
 var which_app_is_chosen: AppsToChose
 
 var text_editor: TextEdit
 var text_editor_control: Control
 
 var calculator_control: Control
+
+var calculator_screen: RichTextLabel
 
 ## Universal UI
 var save_button: Button
@@ -94,7 +120,54 @@ func cb_switch_button() -> void:
 			make_visible(AppsToChose.Notes)
 
 
+func cb_add_text_to_calculator(input: String) -> void:
+	calculator_screen.text += input;
+	pass
+
+
+func cb_remove_text_from_calculator() -> void:
+	calculator_screen.clear()
+
+
+func cb_add_dot_to_calculator() -> void:
+	if calculator_state == CalculatorState.HasDot: return
+	calculator_state = CalculatorState.HasDot
+	calculator_screen.text += "."
+	pass
+
+
+func cb_add_math_operator_to_calculator(which_operator: MathOperator) -> void:
+	calculator_state = CalculatorState.DoesNotHaveDot
+	calculator_screen.text += " " + MathOperatorToSymbol[which_operator] + " "
+	pass
+
+
+func cb_evaluate_expression() -> void:
+	var expr: Expression = Expression.new()
+	var err = expr.parse(calculator_screen.text)
+
+	if err != OK:
+		printerr("Could not execute |%s|" % calculator_screen.text)
+		calculator_screen.text = "Error"
+		return
+	var result: Variant = expr.execute()
+	match typeof(result):
+		TYPE_FLOAT:
+			var final: float = result
+			calculator_screen.text = str(final)
+		TYPE_INT:
+			var final: int = result
+			calculator_screen.text = str(final)
+		_:
+			printerr("INVALID OPERATION")
+			calculator_screen.text = "INVALID OPERATION"
+
+	pass
+
+
 func _set_variables():
+	## calculator state
+	calculator_state = CalculatorState.DoesNotHaveDot
 	## SQL
 	sql_manager = %sql_manager
 	## shared UI
@@ -104,41 +177,60 @@ func _set_variables():
 	## Text Editor
 	text_editor = %text_editor
 	text_editor_control = %text_editor_control
-
 	var rng := RandomNumberGenerator.new()
 	var rand_quote_index: int = rng.randi_range(0, random_quotes.size() - 1)
 	text_editor.placeholder_text = random_quotes[rand_quote_index]
 	## CALCULATOR
 	calculator_control = %calculator_control
+	calculator_screen = % "calculator_screen"
 	# Buttons
-	button_0 = %"0_button"
-	button_1 = %"1_button"
-	button_2 = %"2_button"
-	button_3 = %"3_button"
-	button_4 = %"4_button"
-	button_5 = %"5_button"
-	button_6 = %"6_button"
-	button_7 = %"7_button"
-	button_8 = %"8_button"
-	button_9 = %"9_button"
-	button_dot = %"dot_button"
-	button_equals = %"equal_button"
-	button_plus = %"plus_button"
-	button_minus  = %"minus_button"
-	button_mul = %"mul_button"
-	button_div = %"div_button"
-	button_c = %"c_button"
-	button_ac  = %"ac_button"
-	button_next  = %"next_button"
-	button_prev = %"prev_button"
-
-	
-
-
+	button_0 = % "0_button"
+	button_1 = % "1_button"
+	button_2 = % "2_button"
+	button_3 = % "3_button"
+	button_4 = % "4_button"
+	button_5 = % "5_button"
+	button_6 = % "6_button"
+	button_7 = % "7_button"
+	button_8 = % "8_button"
+	button_9 = % "9_button"
+	button_dot = % "dot_button"
+	button_equals = % "equal_button"
+	button_plus = % "plus_button"
+	button_minus = % "minus_button"
+	button_mul = % "mul_button"
+	button_div = % "div_button"
+	button_c = % "c_button"
+	button_ac = % "ac_button"
+	button_next = % "next_button"
+	button_prev = % "prev_button"
 	pass
 
 
 func _connect_events():
+
+
+	var default_func = func(x: String) -> void:
+		cb_add_text_to_calculator(x);
+		pass
 	save_button.button_down.connect(cb_save_button)
 	load_button.button_down.connect(cb_load_button)
 	switch_button.button_down.connect(cb_switch_button)
+	button_0.button_down.connect(default_func.bind("0"))
+	button_1.button_down.connect(default_func.bind("1"))
+	button_2.button_down.connect(default_func.bind("2"))
+	button_3.button_down.connect(default_func.bind("3"))
+	button_4.button_down.connect(default_func.bind("4"))
+	button_5.button_down.connect(default_func.bind("5"))
+	button_6.button_down.connect(default_func.bind("6"))
+	button_7.button_down.connect(default_func.bind("7"))
+	button_8.button_down.connect(default_func.bind("8"))
+	button_9.button_down.connect(default_func.bind("9"))
+	button_dot.button_down.connect(cb_add_dot_to_calculator)
+	button_plus.button_down.connect(cb_add_math_operator_to_calculator.bind(MathOperator.Plus))
+	button_minus.button_down.connect(cb_add_math_operator_to_calculator.bind(MathOperator.Minus))
+	button_mul.button_down.connect(cb_add_math_operator_to_calculator.bind(MathOperator.Mul))
+	button_div.button_down.connect(cb_add_math_operator_to_calculator.bind(MathOperator.Div))
+	button_equals.button_down.connect(cb_evaluate_expression)
+
+	pass
