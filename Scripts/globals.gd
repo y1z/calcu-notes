a4de760db49a783bdc0693015dd3613bf94bf732
GAME-DEV-YAY\Administrator <zap718@amerike.edu.mx>
2026-06-02 16:03:30 -0500

    Add globals.gd

---
+++ Scripts/globals.gd
@@ -0,0 +1,26 @@
+extends Node
+
+enum CalcButtons 
+{
+	NONE,
+	Button_0,
+	Button_1,
+	Button_2,
+	Button_3,
+	Button_4,
+	Button_5,
+	Button_6,
+	Button_7,
+	Button_8,
+	Button_9,
+	Button_plus,
+	Button_minus,
+	Button_mul,
+	Button_div,
+	Button_equal,
+	Button_dot,
+	Button_c,
+	Button_ac,
+	Button_prev,
+	Button_next,
+}
